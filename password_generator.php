57720df9b951cd5b3da6f24245e84ab155314fed
LordMethenor <lordmethenor@outlook.com>
2020-10-05 16:29:24 +0000

    Select a random character

--- password_generator.php
+++ password_generator.php
@@ -6,5 +6,10 @@ $symbols = '!@#$%^&*()';
 
 $chars = $lower . $upper . $numbers . $symbols;
 
-echo $chars;
+function random_char($string) {
+  $i = random_int(0,strlen($string)-1); //use mtrand if working with an older version or third party library
+  return $string[$i];
+}
+
+echo random_char($chars);
 ?>
