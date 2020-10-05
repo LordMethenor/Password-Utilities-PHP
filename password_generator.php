12c0a949ae51ec84edb5739a6fc4d815d597e44f
LordMethenor <lordmethenor@outlook.com>
2020-10-05 16:34:07 +0000

    Build a randomized string

--- password_generator.php
+++ password_generator.php
@@ -7,9 +7,14 @@ $symbols = '!@#$%^&*()';
 $chars = $lower . $upper . $numbers . $symbols;
 
 function random_char($string) {
-  $i = random_int(0,strlen($string)-1); //use mtrand if working with an older version or third party library
+  $i = random_int(0,strlen($string)-1); //use mtrand if working with an older version or third party library, maybe add param?
   return $string[$i];
 }
-
-echo random_char($chars);
+function random_string($length, $char_set) {
+  for($i=0; $i < $length; $i++) {
+    $output .= random_char($char_set);
+  }
+  return $output;
+}
+echo random_string(8, $chars);
 ?>
