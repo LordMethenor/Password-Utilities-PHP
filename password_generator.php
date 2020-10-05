10eb2e7ac0d21eb9a6cc14855548d3cfc84ea25e
LordMethenor <lordmethenor@outlook.com>
2020-10-05 17:05:38 +0000

    Using a form for configuration

--- password_generator.php
+++ password_generator.php
@@ -18,18 +18,52 @@ function generate_password($length) {
   $symbols = '!@#$%^&*()';
 
   //extract configuration flags into variables
-  $use_lower = true;
-  $use_upper = true;
-  $use_numbers = true;
-  $use_symbols = true;
+  $use_lower = isset($_GET['lower']) ? $_GET['lower'] : '0';;
+  $use_upper = isset($_GET['upper']) ? $_GET['upper'] : '0';;
+  $use_numbers = isset($_GET['numbers']) ? $_GET['numbers'] : '0';;
+  $use_symbols = isset($_GET['symbols']) ? $_GET['symbols'] : '0';;
 
   $chars = '';
-  if($use_lower === true) { $chars .= $lower; }
-  if($use_upper === true) { $chars .= $upper; }
-  if($use_numbers === true) { $chars .= $numbers; }
-  if($use_symbols === true) { $chars .= $symbols; }
+  if($use_lower == '1') { $chars .= $lower; }
+  if($use_upper == '1') { $chars .= $upper; }
+  if($use_numbers == '1') { $chars .= $numbers; }
+  if($use_symbols == '1') { $chars .= $symbols; }
 
   return random_string($length, $chars);
 }
-echo generate_password(8);
+$password = generate_password($_GET['length']);
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Password Generator</title>
+  </head>
+  <body>
+
+    <p> Generated Password: <?php echo $password; ?></p>
+
+    <p>Generate a new password using the form options.</>
+    <form action="" method="get">
+    <!--Loop the checkboxes and grab the names from array,re-do the save state checking with php functions-->
+      Length: <input type="text" name="length" value= 
+        "<?php if(isset($_GET['length'])) { echo $_GET['length']; } ?>"
+       /><br />
+      <!--use number line-->
+      <input type="checkbox" name="lower" value="1" 
+        <?php if($_GET['lower'] == 1) {echo 'checked';}?> 
+      /> Lowercase<br />
+      <input type="checkbox" name="upper" value="1" 
+        <?php if($_GET['upper'] == 1) {echo 'checked';}?> 
+      /> Uppercase<br />
+      <input type="checkbox" name="number" value="1" 
+        <?php if($_GET['number'] == 1) {echo 'checked';}?> 
+      /> Number<br />
+      <input type="checkbox" name="symbols" value="1" 
+        <?php if($_GET['symbols'] == 1) {echo 'checked';}?> 
+      /> Symbols<br />
+      <input type="submit" value="Submit" />
+    </form>
+  </body>
+</html>
