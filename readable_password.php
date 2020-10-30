3927d71fb76d36708db9bcca5dbb1f591be73eb0
LordMethenor <lordmethenor@outlook.com>
2020-10-30 20:42:44 +0000

    Add random numbers and symbols

--- readable_password.php
+++ readable_password.php
@@ -10,14 +10,27 @@ function pick_random($array) {
   $i = random_int(0, count($array)-1);
   return $array[$i];
 }
+
+function pick_random_symbol() {
+  //unify symbols for all instances
+  $symbols = '$*?!-';
+  $i = random_int(0, strlen($symbols) -1);
+  return $symbols[$i];
+}
+
+function pick_random_number($digits=1) {
+  $min = pow(10, ($digits -1));
+  $max = pow(10, $digits) - 1;
+  return strval(random_int($min,$max));
+}
 $basic_words = read_dictionary('friendly_words.txt');
 $brand_words = read_dictionary('brand_words.txt');
 
 $words = array_merge($brand_words, $basic_words);
 // could use array_unique to prevent any repeats
-
-for ($i = 0; $i < 3; $i++) {
-  $password = $password . pick_random($words);
-}
+$password = pick_random($words);
+$password .= pick_random_symbol();
+$password .= pick_random_number(3);
+$password .= pick_random($words);
 echo "Friendly password: " . $password . "<br />";
 ?>
