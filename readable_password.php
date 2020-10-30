59f7ee4b548cb9ffe274c67300d5538a2a5e0bee
LordMethenor <lordmethenor@outlook.com>
2020-10-30 20:20:10 +0000

    Merging Dictionaries

--- readable_password.php
+++ readable_password.php
@@ -1,5 +1,17 @@
 <?php
-//make sure space at end of dictionary isn't used, multi dictionary support
-$dictionary_file = 'dictionaries/friendly_words.txt';
-$lines = file($dictionary_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+//make multi dictionary support
+function read_dictionary($filename="") {
+  $dictionary_file = "dictionaries/{$filename}";
+  return file($dictionary_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+}
+
+$basic_words = read_dictionary('friendly_words.txt');
+$brand_words = read_dictionary('brand_words.txt');
+
+$words = array_merge($brand_words, $basic_words);
+// could use array_unique to prevent any repeats
+
+for ($i=0; $i<8; $i++) {
+  echo $words[$i] . "<br />";
+}
 ?>
